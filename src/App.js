aef48d7b2604c213eee22ee76b8bb4b7c5e2efd3
nazaninbinesh <nazanin_binesh@yahoo.com>
2021-03-14 22:02:06 -0700

    add inventory basic component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import React, { useState } from "react";
+import InventoryNbi67 from "./ComponentsNbi67/InventoryNbi67/InventoryNbi67";
 
 function App() {
   return (
     <div className="App">
-      <h1>app page</h1>
+      <div className="container">
+        <InventoryNbi67 />
+      </div>
     </div>
   );
 }
---
+++ src/ComponentsNbi67/InventoryNbi67/InventoryNbi67.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function InventoryNbi67() {
+  return (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">SKU</th>
+          <th scope="col">Name</th>
+          <th scope="col">Quantity</th>
+          <th scope="col">Price</th>
+          <th scope="col">Add</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>3</td>
+          <td>$2.99</td>
+          <td>
+              <button className="btn btn-success">Add</button>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>5</td>
+          <td>$27.32</td>
+          <td>
+              <button className="btn btn-success">Add</button>
+          </td>
+        </tr>      
+      </tbody>
+    </table>
+  );
+}
+
+export default InventoryNbi67;
