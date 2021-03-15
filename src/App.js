622a9d76f4d4d4d15ef7ffa9e2e04d444dfaea73
nazaninbinesh <nazanin_binesh@yahoo.com>
2021-03-14 22:09:42 -0700

    Cart Basic Component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import InventoryNbi67 from "./ComponentsNbi67/InventoryNbi67/InventoryNbi67";
+import CartNbi67 from "./ComponentsNbi67/CartNbi67/CartNbi67"
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <InventoryNbi67 />
+        <CartNbi67 />
       </div>
     </div>
   );
